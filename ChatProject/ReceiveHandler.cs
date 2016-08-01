2b9ba7f315511225f6a37755ab6da28e966d41bb
JinHyuk-Lee <obital59@gmail.com>
2016-08-01 07:43:10 +0100

    v1 ReceiveHandler

--- ChatProject/ReceiveHandler.cs
+++ ChatProject/ReceiveHandler.cs
@@ -20,11 +20,101 @@ namespace Ircc
             {
                 switch(recvPacket.header.code)
                 {
-                    //Received SignUp Request
+                    //------------CREATE------------
+                    case Code.CREATE:
+                        //FE side
+                        break;
+                    case Code.CREATE_DUPLICATE_ERR:
+                        //CL side
+                        break;
+                    case Code.CREATE_FULL_ERR:
+                        //CL side
+                        break;
+
+
+                    //------------DESTROY------------
+                    case Code.DESTROY:
+                        //FE side
+                        break;
+                    case Code.DESTROY_ERR:
+                        //CL side
+                        break;                        
+
+
+                    //------------FAIL------------
+                    case Code.FAIL:
+                        //FE side
+                        break;
+
+
+                    //------------HEARTBEAT------------
+                    case Code.HEARTBEAT:
+                        //CL side
+                        break;
+                    case Code.HEARTBEAT_RES:
+                        //FE side
+                        break;
+
+
+                    //------------JOIN------------
+                    case Code.JOIN:
+                        //FE side
+                        break;
+                    case Code.JOIN_FULL_ERR:
+                        //CL side
+                        break;
+                    case Code.JOIN_NULL_ERR:
+                        //CL side
+                        break;                        
+
+
+                    //------------LEAVE------------
+                    case Code.LEAVE:
+                        //FE side
+                        break;
+                    case Code.LEAVE_ERR:
+                        //CL side
+                        break;
+
+
+                    //------------LIST------------
+                    case Code.LIST:
+                        //FE side
+                        break;
+                    case Code.LIST_ERR:
+                        //CL side
+                        break;
+                    case Code.LIST_RES:
+                        //CL side
+                        break;                    
+
+
+                    //------------MSG------------
+                    case Code.MSG:
+                        //CL and FE side
+                        break;
+                    case Code.MSG_ERR:
+                        //CL and FE side
+                        break;
+
+
+                    //------------SIGNIN------------
+                    case Code.SIGNIN:
+                        //FE and BE side
+                        break;
+                    case Code.SIGNIN_ERR:
+                        //CL side
+                        break;
+                    case Code.SIGNIN_RES:
+                        //CL side
+                        break;
+
+
+                    //------------SIGNUP------------
                     case Code.SIGNUP:
                         //FE and BE side
                         //Do SignUp Process                        
-                        break;                    
+                        break;
                     case Code.SIGNUP_ERR:
                         //CL side
                         //error handling
@@ -34,22 +124,57 @@ namespace Ircc
                         //success
                         break;
 
-                    case Code.LOGIN:
-                        //FE and BE side
-                        break;
-                    case Code.LOGIN_ERR:
-                        //CL side
-                        break;
-                    case Code.LOGIN_RES:
-                        //CL side
-                        break;
+
+                    //------------SUCCESS------------
+                    case Code.SUCCESS:
+                        //FE side
+                        break;                    
                 }
 
             }
             //Server to Server Side
             else if(Comm.SS == recvPacket.header.comm)
             {
+                switch (recvPacket.header.code)
+                {
+                    //------------SDESTROY------------
+                    case Code.SDESTROY:
+                        //FE side
+                        break;
+                    case Code.SDESTROY_ERR:
+                        //FE side
+                        break;
 
+
+                    //------------SJOIN------------
+                    case Code.SJOIN:
+                        //FE side
+                        break;
+                    case Code.SJOIN_ERR:
+                        //FE side
+                        break;
+
+
+                    //------------SLIST------------
+                    case Code.SLIST:
+                        //FE side
+                        break;
+                    case Code.SLIST_ERR:
+                        //FE side
+                        break;
+                    case Code.SLIST_RES:
+                        //FE side
+                        break;
+
+
+                    //------------SMSG------------                
+                    case Code.SMSG:
+                        //FE side
+                        break;
+                    case Code.SMSG_ERR:
+                        //FE side
+                        break;
+                }
             }
             //Dummy to Server Side
             else if (Comm.DUMMY == recvPacket.header.comm)
@@ -57,7 +182,10 @@ namespace Ircc
 
             }
 
-                Packet returnPacket = new Packet(returnHeader,returnData);
+
+
+            Packet returnPacket = new Packet(returnHeader,returnData);
+
             return new Packet();
         }
     }
